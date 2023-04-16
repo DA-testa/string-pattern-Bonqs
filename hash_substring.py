d18d3b83b3509f818203e8399a1df90ca67a628e
221RDB243 <125822361+Bonqs@users.noreply.github.com>
2023-04-16 22:56:54 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,4 +1,7 @@
-def r():return(input().strip(),input().strip())if input().startswith('F'):with open('./tests/06') as f: x,t=map(str.strip,f.readlines())else(input().strip(),input().strip())
-def l(o):print(*o)
-def g(x,t):return[i for i in range(len(t)-len(x)+1)if t[i:i+len(x)]==x]
-if __name__=='__main__':l(g(*r()))
+def find_occurrences():
+    text = input().strip().split()
+    pattern = text[0]
+    string = text[1]
+    return [i for i in range(len(string)-len(pattern)+1) if string[i:i+len(pattern)] == pattern]
+if __name__ == '__main__':
+    print(*find_occurrences())
