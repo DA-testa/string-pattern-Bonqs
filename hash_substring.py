7bc48c40e9c0a10bd385f3162d14059451654326
221RDB243 <125822361+Bonqs@users.noreply.github.com>
2023-04-16 22:36:30 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,16 +1,9 @@
-def read_input():
+def r():
     p = input()
     if p[0] == 'F':
-        with open('./tests/06', 'r') as f:
-            x, t = map(str.strip, f.readlines())
-    elif p[0] == 'I':
-        x = input().strip()
-        t = input().strip()
-    return x, t
-def l(o):
-    print(*o)
-def g(x, t):
-    c = [i for i in range(len(t) - len(x) + 1) if t[i:i+len(x)] == x]
-    return c
-if __name__ == '__main__':
-    l(g(*read_input()))
+        with open('./tests/06') as f: x,t=map(str.strip,f.readlines())
+    else: x,t=input().strip(),input().strip()
+    return x,t
+def l(o): print(*o)
+def g(x,t): return[i for i in range(len(t)-len(x)+1)if t[i:i+len(x)]==x]
+if __name__=='__main__': l(g(*r()))
