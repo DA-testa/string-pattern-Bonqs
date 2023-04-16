244cddb648ba5aac7152dffd6a98bf566c2cdeca
221RDB243 <125822361+Bonqs@users.noreply.github.com>
2023-04-16 22:42:52 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,5 +1,9 @@
 def r():
- p=input();x,t=(input if p[0]!='F' else lambda:open('./tests/06')).strip().readlines();return x.strip(),t.strip()
-l=lambda o:print(*o)
-g=lambda x,t:[i for i in range(len(t)-len(x)+1)if t[i:i+len(x)]==x]
-if __name__=='__main__':l(g(*r()))
+    p = input()
+    if p[0] == 'F':
+        with open('./tests/06') as f: x,t=map(str.strip,f.readlines())
+    else: x,t=input().strip(),input().strip()
+    return x,t
+def l(o): print(*o)
+def g(x,t): return[i for i in range(len(t)-len(x)+1)if t[i:i+len(x)]==x]
+if __name__=='__main__': l(g(*r()))
